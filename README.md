c1e347dfc7eefdef15997609f8e2b3c9b4dcca41
HaiNT-Teko <hai.nt@teko.vn>
2019-11-17 15:28:02 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# python-kongclient
+## Python binding to the Kong API
+
+This is a client for the KOng API. It provides a Python API (the `kongclient` module).
+
+
+## Installation
+
+## Manual
+
+
+## Instructions
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Kong-Client',
+    version='1.1.0',
+    author='HaiNT',
+    author_email='tronghaibk2008@gmail.com',
+    description='A python client for the Kong API',
+    long_description=long_description,
+    url='https://github.com/haintd/python-kongclient',
+    packages=['kongclient'],
+    install_requires=['requests'],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.6'
+)
